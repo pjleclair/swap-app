7347f2d5210dd15c9a2a4f3e1cbd85d0317e1432
lompdoctor <pleclair@protonmail.com>
2023-02-10 16:29:59 -0600

    Scaffold & style Swap Header

--- src/components/swap/swap.js
+++ src/components/swap/swap.js
@@ -1,9 +1,22 @@
 import "./swap.css"
+import msgImg from "./images/Message.svg"
+import notifImg from "./images/Notification.svg"
+import userImg from "./images/User.svg"
 
 const Swap = () => {
     return(
         <div className="swap--container">
-
+            <div className="swap--header">
+                <input id="search" type="search" placeholder="Search..."/>
+                <img src={msgImg} alt="messages"/>
+                <img src={notifImg} alt="notifs"/>
+                <img id="user" src={userImg} alt="user"/>
+            </div>
+            <div className="convert--container">
+                <div id="label">Convert</div>
+                <div className="swap--convert"></div>
+            </div>
+            <div className="swap--assets"></div>
         </div>
     )
 }
